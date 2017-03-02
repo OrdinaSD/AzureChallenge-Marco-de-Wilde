37608e3699f490578a706ce9198f2c6bdc399dcd
Unknown <marco.de.wilde@ordina.nl>
2017-03-02 19:56:43 +0000

    Nieuw

---
+++ MarcoDeWildeSocialNetwork/MarcoDeWildeSocialNetwork.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MarcoDeWildeSocialNetwork.Web.Startup))]
+namespace MarcoDeWildeSocialNetwork.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
